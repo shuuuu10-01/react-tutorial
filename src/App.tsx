35f73f55f77970d30ac3c70fead56c5dba223868
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-01-26 12:27:35 +0900

    [update] header,main,footerの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
@@ -10,23 +9,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Header</p>
+      </header>
+      <main className="App-main">
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/About" component={About} />
           </Switch>
         </Router>
-      </header>
+      </main>
+      <footer>
+        <p>2021©shuuuu</p>
+      </footer>
     </div>
   );
 }
