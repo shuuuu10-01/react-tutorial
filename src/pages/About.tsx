ce837c1852dc85edaf13b4169e3d8ea31957279b
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-01-30 23:00:58 +0900

    [update] inputの使い方

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 
+interface inputState {
+  text: string;
+}
+
 class Home extends React.Component {
+  state: inputState = {
+    text: "",
+  };
+  private changeText(e: any): void {
+    this.setState({text:e.target.value})
+  }
   render() {
     return (
       <div>
-        <p>this is aboutpage</p>
+        <p>
+          ここに記入してください
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={(e) => this.changeText(e)}
+          ></input>
+        </p>
+        <p>{this.state.text}</p>
       </div>
     );
   }
